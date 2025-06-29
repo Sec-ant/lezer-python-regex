f249bbe45e0ce349068575a8a0c898900c16e503
Ze-Zheng Wu <zezhengwu@proton.me>
2025-06-30 01:31:21 +0800

    feat: add html page for vite dev

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { LRLanguage } from "@codemirror/language";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { parser } from "./src/index.ts";
+
+const pythonRegexLanguage = LRLanguage.define({
+  parser,
+  languageData: { name: "python-regex" },
+});
+
+new EditorView({
+  doc: "(?i)^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$",
+  parent: document.body,
+  extensions: [basicSetup, pythonRegexLanguage],
+});
